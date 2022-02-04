e43a1ffba8985d09d5942e08b49a3dc7a10c1973
lukden <dean.l@northeastern.edu>
2022-02-03 21:45:29 -0500

    add li to save user input

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
-// console.log('sanity check')
+// // console.log('sanity check')
 
 const inputElement = document.querySelector("#new-input")
 const btn = document.querySelector("#submit-button")
 const ulEl = document.querySelector("#todo-list")
+
+btn.addEventListener('click', function(event) {
+  if(inputElement.value === '') return
+  const newLi = document.createElement('li')
+  newLi.textContent = inputElement.value
+  ulEl.appendChild(newLi)
+  inputElement.value = ''
+})
+
+
+
