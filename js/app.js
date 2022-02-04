4fc453503325c0c3e49c422ae4c603f66be5a6ec
lukden <dean.l@northeastern.edu>
2022-02-03 20:43:59 -0500

    add cached elements

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// console.log('sanity check')
+
+const inputElement = document.querySelector("#new-input")
+const btn = document.querySelector("#submit-button")
+const ulEl = document.querySelector("#todo-list")
